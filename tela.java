68d259636e28a7f21c8c3b6381797b3aea173f73
HL-Neto <162185205+HL-Neto@users.noreply.github.com>
2025-05-09 09:40:51 -0300

    Create tela.java

---
+++ tela.java
@@ -0,0 +1,93 @@
+package JAVA.Spacewar;
+
+import java.util.*;
+
+public class tela {
+    
+
+    // configuração da matriz
+    
+    int linha = 5;
+    int coluna = 3;
+
+   
+
+    // posiçao do player / posição inicial
+    input posp = new input();
+   
+    int pospL = 4;
+    int pospC = 1;
+    
+    
+    
+    
+    public tela(){
+        
+        
+        // auto explicatico, puxa a função update 
+        updt();
+
+
+        
+        // indices
+        int l;
+        int c;
+        
+        
+
+        System.out.print("\033[H\033[2J"); // limpam o console 
+        System.out.flush();
+        
+        String[][] matriz = new String [linha][coluna]; // criaçãoda matriz
+        
+
+        // deixa os espaços em branco
+        for ( l = 0; l < linha; l++){
+            for (  c = 0; c < coluna; c++ ){
+
+                
+                matriz[l][c] = " ";
+                
+                
+            }
+        }
+
+        
+        //posição da nave -- e alterada com updt
+        matriz[pospL][pospC] = "^";
+        
+        
+        // adiciona as linhas laterais
+        for ( l = 0; l < linha ; l++){
+            System.out.print("|");
+            for( c= 0;  c < coluna; c++){
+                System.out.print(" "+matriz[l][c]+" ");
+            }
+        
+            System.out.println("|");
+        }
+        
+       
+
+      
+    }
+
+    
+    public void updt(){ // atualizar a tela etc etc
+        
+        if ( posp.mid == true){
+            pospC = 1;
+        }
+        if ( posp.esq == true){
+            pospC = 0;
+        }
+        if ( posp.dir == true){
+            pospC = 2;
+        }
+        
+        
+    }
+
+}
+    
+
