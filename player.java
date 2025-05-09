6b2a58737694681c0e6db2d510f770b1b2b387de
HL-Neto <162185205+HL-Neto@users.noreply.github.com>
2025-05-09 12:41:35 -0300

    Create player.java

---
+++ player.java
@@ -0,0 +1,10 @@
+package JAVA.Spacewar;
+
+public class player {
+   
+    String pNome;
+
+    int pScore;
+    
+    int pVida; // n sei se vai ter como o player "morrer", ent isso fica por enquanto definido como um fim de jogo
+}
