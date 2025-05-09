908b01b0c38fb70707d64b5b3b4511e5c4b5f0e2
HL-Neto <162185205+HL-Neto@users.noreply.github.com>
2025-05-09 09:39:55 -0300

    Create SpacewarMain.java

---
+++ SpacewarMain.java
@@ -0,0 +1,23 @@
+package JAVA.Spacewar;
+
+import java.util.*;
+
+public class SpacewarMain {
+    
+
+
+    public static void main(String[] args) {
+        
+        Scanner scaner = new Scanner(System.in);
+        
+        boolean t = true; //loop de teste , mais tarde a gente muda isso pra endgame
+        
+        while (t == true) {
+            tela termminal = new tela();
+        }
+        
+
+        
+
+    }
+}
