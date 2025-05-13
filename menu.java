a7bed65220f19c06aff446b813ba1404d63501bc
HL-Neto <162185205+HL-Neto@users.noreply.github.com>
2025-05-13 09:33:01 -0300

    Update menu.java

--- menu.java
+++ menu.java
@@ -1,85 +1,68 @@
-package JAVA.BOND;
+package JAVA.bond;
 
 import java.util.Scanner;
 
+
 public class menu {
-    
+
     Scanner scaaner = new Scanner(System.in);
     
     public menu(){
 
+    	 System.out.println("\n== < \033[1m007\033[0m > =="); 
+    	 System.out.println("\n    start");
+         System.out.println("\n    score");
+         System.out.println("\n    exit");
+         System.out.print("\n\n\n: ");
          
-
-
-        System.out.println("\n== < \033[1m007\033[0m > ==");   // titulo - // Código ANSI para negrito, tudo que tiver dentro deles fica em negrito
-
-        System.out.println("\n\n start");
-        System.out.println("\n\n score");
-        System.out.println("\n\n exit");
-
         String escolha = scaaner.nextLine();
         
+    	switch (escolha) {
+    	case "start":
+    		play();
+    		break;
+    	case "score":
+    		System.out.println("\n    <- PLACAR ->");
 
-        if (escolha.equals("start")) {
-
-                play();
-            
-            } 
-        
+            System.out.println ( "\n1 - HUMBERTÃO GOSTOSÃO");
+    		break;
+    	case "exit":
+    		System.out.println("exit");
+    		break;
+    	default:
+    		System.out.println("Opcao invalida");
+    	}
+    	scaaner.close();
     }
+  
 
+
+      
+        
+
+        
 
 
 
     
     public void play(){
-            
-        System.out.print("nome:");
-        
-        String input = scaaner.nextLine(); // agente tem q arrumar essa variavel de nome
-            
-            if (input.length() < 4){
-                
 
-                System.out.println("\nplay");   
-                System.out.println("voltar");
-                String inpuut = scaaner.nextLine();
-                System.out.println("carregando...");
-                System.out.println(" 'DIGITE ALGUMA COISA PRA INICIAR'");
-
-                // da dando erro aqui, ta precisando de 2 inputs pra poder seguir com o jogo
-
-                if (inpuut.equals("play")){ // se conseguir tirar o if e colocar o swhitch case 
-                                
-
-                    // switch (inpuut) , ai c colocar as opçoes como instrings, tava como switch case, mas eu tentei cooncertar um erro ent coloquei como if, mas n mudou porra nenhuma, ent se conseguir colocar de volta pra switch
-                        
-                        
-
-                        boolean t = true; // loop para o jogo rodar, vamos substitur t pela vida
-
-                        while ( t = true){
-                        
-                            new tela();
-                        
-                        }
-                    }
-                        
-                    if (inpuut.equals("menu")){
-                        new menu();
-                        // finaliza esse menu e vai pro anterior
-                };
-                
-            
-                
-
-            } else {
-                
-                System.out.print("o nome deve ter ate 3 characters");
-            
-            }
+        System.out.println("\n play");   
+		System.out.println("\n voltar");
+		
+		String escolha2 = scaaner.nextLine();
+		
+		switch (escolha2) {
+		case "play":
+			new tela();
+			break;
+		case "voltar":
+			new menu();
+			break;
+		default:
+			System.out.println("Opcao invalida");
+		}
             
                
     }
 }
-
