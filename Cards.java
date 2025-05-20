b14bf00b673c716accfd9e4ea663f6d92965469c
HL-Neto <162185205+HL-Neto@users.noreply.github.com>
2025-05-20 16:40:21 -0300

    Create Cards.java

---
+++ Cards.java
@@ -0,0 +1,39 @@
+package JAVA.bond;
+
+public class Cards {
+
+    
+    
+    public void extraShild(){
+        //extraSuit
+        //escudo extra
+    }
+
+    public void imunidade(){
+        //wonderGuard
+        //imunidade por um turno
+    }
+
+    public void heal(){
+        //estusFlask
+        // cura
+    }
+
+    public void doubleDMG(){
+        // shotgun
+        // tiro 2x dmg 
+    }
+
+    public void extraAmmo(){
+         //preparetion
+        // bala extra
+    }
+
+    public void curseAmmo(){
+        // - bala anemy
+    }
+    
+
+
+    
+}
