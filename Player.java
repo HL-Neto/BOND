9563d440bc9a1c25e067983c118c357d297179d0
HL-Neto <162185205+HL-Neto@users.noreply.github.com>
2025-05-28 20:23:30 -0300

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,61 +1,90 @@
-package JAVA.bond;
+package bond;
 
 import java.util.Scanner;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 
 public class Player {
     
-    private int vida = 100, ammo = 0, shild = 0;
-    private String playerNome = "pedro";
+    public int vida = 100, ammo = 100, shild = 100;
+
+    public boolean atirar = false , defender = false , municao = false;
+
+    // TESTE //
+     public void keyPressed(KeyEvent e) {
+       
+        int code = e.getKeyCode();
+        
+        
+        if(code == KeyEvent.VK_W){
+            atirar = true;
+
+            SHT();
+        }
+
+        if(code == KeyEvent.VK_Q){
+            defender = true;
+
+            DEF();
+        }
+
+        if(code == KeyEvent.VK_E){
+            municao = true;
+
+            REL();
+        }
 
 
+      
+        
+    }
+
+    public Player(){
+       
+    }
+    //       //
     public int REL(){
 
-        return this.ammo++;
+        if (ammo = 4){
+
+            System.out.println("balas carregadas ao maximo");
+
+            return ammo;
+        }
+        return ammo++;
     }
 
     public int DEF(){
 
-        return this.shild++;
+        if( shild = 4){
+
+            System.out.println("escudos carregados ao maximo");
+
+            return shild;
+        }
+        return shild++;
     }
 
     public int SHT(){
 
-        return this.vida--;
-    }
+        if( ammo < 0 ){
 
-    public static void main(String[] args) {
-        
-        Scanner scaner = new Scanner(System.in);
-        Player player = new Player();
-
-        boolean t = false;
-
-        while (t == false){
-
-            System.out.println("1 - ATIRAR ; 2 - RECARREGAR ; 3 - DEFENDER");
-            System.out.println(player.vida);
-             System.out.println(player.ammo);
-              System.out.println(player.shild);
-            
-            int input  = scaner.nextInt();
-
-            switch (input){
-                case 1:
-
-                    player.SHT();
-                    break;
-
-                case 2:
-
-                    player.REL();
-                    break;
-
-                case 3:
-
-                    player.DEF();
-                    break;
-            }
+            System.out.println("sem balas chefe  "+ ammo);
+            return ammo;
         }
+
+
+        if (shild > 0){
+
+            shild -- ;
+
+            return ammo-- ;
+        } else {
+
+            vida -- ;
+        }
+        return ammo-- ; 
+        
     }
 }
