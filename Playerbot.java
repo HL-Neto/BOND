f7cdf5469b9feebe9f9ee00386a93b9151679639
HL-Neto <162185205+HL-Neto@users.noreply.github.com>
2025-06-02 17:57:13 -0300

    Update Playerbot.java

--- Playerbot.java
+++ Playerbot.java
@@ -5,30 +5,48 @@ import java.util.Random;
 public class Playerbot extends Playerconfig {
 	private Random random;
 
-	public Playerbot() {
+    boolean botshoot = false, botdef = false, botrell = false;
+
+
+	
+    public Playerbot() {
 		super(0, 0, 3);
 		this.random = new Random();
-		// TODO Auto-generated constructor stub -- isso aqui foi gpt?
+		
 	}
 	
 	public void acaobot() {
+        
         int acao = random.nextInt(3);
 
+        // precisa disso pra zerar os estados anteriores
+        botshoot = false; 
+        botdef = false; 
+        botrell = false;
+
+       
         switch (acao) {
             case 0:
+                
+                
+                botshoot = true;
                 SHT();
+                
                 break;
             case 1:
+
+                botdef = true;
                 DEF();
+                
                 break;
             case 2:
+
+                botrell = true;
                 REL();
+                
                 break;
         }
     }
 }
 	
 	
-	
-
-
