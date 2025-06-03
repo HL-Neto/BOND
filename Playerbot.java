d79fed5d343b50311a6b884204b8d17fcff43b02
HL-Neto <162185205+HL-Neto@users.noreply.github.com>
2025-06-03 17:01:22 -0300

    Update Playerbot.java

--- Playerbot.java
+++ Playerbot.java
@@ -2,33 +2,73 @@
 
 import java.util.Random;
 
+import javax.swing.Timer;
+
 public class Playerbot extends Playerconfig {
 	private Random random;
 
-	public Playerbot() {
+    boolean botshoot = false, botdef = false, botrell = false;
+
+     
+
+	
+    public Playerbot() {
 		super(0, 0, 3);
 		this.random = new Random();
-		// TODO Auto-generated constructor stub -- isso aqui foi gpt?
+		
 	}
 	
 	public void acaobot() {
+        
         int acao = random.nextInt(3);
 
+        // precisa disso pra zerar os estados anteriores
+        botshoot = false; 
+        botdef = false; 
+        botrell = false;
+
+        
+
+        if (actCD) return;
+        
         switch (acao) {
             case 0:
-                SHT();
+                
+                
+                botshoot = true;
+                this.SHT(new Player());
+                
                 break;
             case 1:
-                DEF();
+
+                botdef = true;
+                this.DEF();
+                
                 break;
             case 2:
-                REL();
+
+                botrell = true;
+                this.REL();
+                
                 break;
+        
+            }
+        actCD = true;
+        
+        if (countdown != null && countdown.isRunning()){
+
+                countdown.stop(); // Para um time novo caso ja tenha outro rolando
         }
+        
+        countdown = new Timer(5000, evt -> {
+            System.out.println("NOVO TURNO!!");
+            actCD = false;
+        });
+        
+        countdown.setRepeats(false); // opcional, para evitar repetições
+        countdown.start();
+    
     }
 }
 	
 	
-	
-
-
